d909c078f19ab22fbc7bc7717d9730c34575283b
Andre Odendaal <aodendaal@gmail.com>
2022-08-01 21:20:38 +0200

    Add heist prefabs

---
+++ Assets/Scripts/EnemyNavMesh.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavMesh : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+
+    private NavMeshAgent navMeshAgent;
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.destination = playerTransform.position;
+    }
+}
